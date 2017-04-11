0d605d858daa83000e05718a8692748170c1d6d2
Miriam Elizabeth Laguna Velazquez <Miriam Elizabeth Laguna Velazquez>
2017-04-11 12:22:44 -0500

    super editado

---
+++ canchas.js
@@ -0,0 +1,17 @@
+var botonFut = document.getElementById("botonFutbol");
+var botonFut = document.getElementById("botonBasquetball");
+var botonFut = document.getElementById("botonTennis");
+var canchaFut = document.getElementById("futbol");
+var canchaFut = document.getElementById("botonBasquetball");
+var canchaFut = document.getElementById("tennis");
+
+function mostrarCancha (cancha) {
+}
+
+
+function ocultarCancha(cancha) {
+}
+
+botonFutbol.addEventListener("click",detectarBoton);
+botonBasquetball.addEventListener("click", detectarBoton);
+botonTennis.addEventListener("click", detectarBoton);
